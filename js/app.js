0c8e9fe535f6a1d57df9a3d1466fa46f3cc8a94f
Jhosep Mena <jhosepmena@gmail.com>
2018-09-11 14:28:32 -0700

    Initial Commit

---
+++ js/app.js
@@ -0,0 +1,21 @@
+'use strict';
+
+var game = prompt('Lets play a game! Answer yes or no, click ok!');
+console.log(game);
+var gameresponse = 'Awesome lets start!';
+var gameresponseno = 'Too bad, welcome to the matrix...';
+
+if (game === 'yes') {
+  alert(gameresponse);} else { alert(gameresponseno);
+}
+
+
+
+
+var pill = prompt('Which pill will you choose red or blue?');
+console.log(game);
+var pillred = 'Welcome to the rabbit lets see how far it goes...';
+var pillblue = 'I see you choose to wake up... not today';
+
+if (pill === 'yes') {
+  alert(pillred);} else { alert(pillblue); }
