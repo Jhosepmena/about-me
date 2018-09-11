a1a6f30aa749e4695b276054054ba8b263eaf2af
Jhosep Mena <jhosepmena@gmail.com>
2018-09-11 15:11:38 -0700

    Added JS prompts to game

--- js/app.js
+++ js/app.js
@@ -1,21 +1,43 @@
 'use strict';
 
 var game = prompt('Lets play a game! Answer yes or no, click ok!');
-console.log(game);
+console.log(game.toUpperCase());
 var gameresponse = 'Awesome lets start!';
-var gameresponseno = 'Too bad, welcome to the matrix...';
+var gameresponseno = 'Well lets give it a try anyway..';
 
 if (game === 'yes') {
   alert(gameresponse);} else { alert(gameresponseno);
 }
 
 
+var born = prompt('Was I born in the state?');
+console.log(born.toUpperCase());
+var bornyes = 'Actually I was born in Venezuela';
+var bornno = 'You are right I was born in Venezuela';
+
+if (born === 'yes') {
+  alert(bornyes);} else { alert(bornno); }
 
 
-var pill = prompt('Which pill will you choose red or blue?');
-console.log(game);
-var pillred = 'Welcome to the rabbit lets see how far it goes...';
-var pillblue = 'I see you choose to wake up... not today';
+var music = prompt('Do I have music tatoos?');
+console.log(music.toUpperCase());
+var musicyes = 'Yes! I got them very late at night for no reason';
+var musicno = 'I actually do, I have them on my left arm';
 
-if (pill === 'yes') {
-  alert(pillred);} else { alert(pillblue); }
+if (music === 'yes') {
+  alert(musicyes);} else { alert(musicno); }
+
+
+var running = prompt('Have I ever ran a Ultramarathon?');
+console.log(running.toUpperCase());
+var runningyes = 'I did... Never again';
+var runningno = 'Once a long time ago I did, but like I said a long time ago';
+
+if (running === 'yes') {
+  alert(runningyes);} else { alert(runningno); }
+
+
+
+
+
+
